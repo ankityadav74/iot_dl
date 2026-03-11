bfd41e5eeef0e219c65c06599d69c7a10eb03d02
Amlan <amlansarkar131@gmail.com>
2026-03-11 23:47:19 +0530

    Add configuration file for WSN_DL

---
+++ 01_WSN_DL/config.js
@@ -0,0 +1,9 @@
+{
+    "timestamp": "2026_03_11__23_15_08",
+    "run_dir": "/root/amlan/Iot/runs/2026_03_11__23_15_08",
+    "pytorch": "2.10.0+cu128",
+    "device": "cuda:1",
+    "cuda": true,
+    "xgboost": true,
+    "shap": true
+}
